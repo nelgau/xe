e33107d611afa8b3f6aa614fd9d738a54118cdb6
Nelson Gauthier <nelson@airbnb.com>
2014-05-09 19:30:37 -0700

    Proxy invalidation

--- lib/xe/errors.rb
+++ lib/xe/errors.rb
@@ -2,10 +2,16 @@ module Xe
   # The superclass from which all Xe errors descend.
   class Error < StandardError; end
 
-  # Raised when an attempt is made to enumerate outside of an active context.
+  # Raised when attempting to enumerate outside of an active context.
   class NoContextError < Error; end
+  # Raised when an inconsistent state is detected during context invalidation.
+  class InconsistentContextError < Error; end
+
   # Raised when a client attempts to defer realization on a disabled context.
   class DeferError < Error; end
-  # Raised when there are waiting fibers but no deferrals to resolve.
+  # Raised when attempting to resolve an invalid proxy.
+  class InvalidProxyError < Error; end
+
+  # Raised when there are waiting fibers but no values left to realize.
   class DeadlockError < Error; end
 end
