e944739b27a85e10d8a1005c6cae0b42309d0178
Nelson Gauthier <nelson@airbnb.com>
2014-05-12 03:28:28 -0700

    Add specs for strategies

--- spec/lib/xe/enumerator/strategy/base_spec.rb
+++ spec/lib/xe/enumerator/strategy/base_spec.rb
@@ -44,4 +44,20 @@ describe Xe::Enumerator::Strategy::Base do
 
   end
 
+  describe '#inspect' do
+
+    it "is a string" do
+      expect(subject.inspect).to be_an_instance_of(String)
+    end
+
+  end
+
+  describe '#to_s' do
+
+    it "is a string" do
+      expect(subject.to_s).to be_an_instance_of(String)
+    end
+
+  end
+
 end
--- spec/lib/xe/enumerator/strategy/evaluator_spec.rb
+++ spec/lib/xe/enumerator/strategy/evaluator_spec.rb
@@ -5,14 +5,27 @@ describe Xe::Enumerator::Strategy::Evaluator do
 
   subject { Xe::Enumerator::Strategy::Evaluator.new(context, &value_proc) }
 
-  let(:context)    { new_context_mock(&finalize_proc) }
-  let(:deferrable) { Xe::Deferrable.new }
-
-  let(:finalize_proc) { Proc.new {} }
+  let(:context)       { new_context_mock(&finalize_proc) }
   let(:value_proc)    { Proc.new { value } }
+  let(:finalize_proc) { Proc.new {} }
   let(:value)         { 4 }
 
+  let(:deferrable)    { Xe::Deferrable.new }
+
+  let(:wait_target)   { Xe::Target.new(deferrable, 0, 0) }
+  let(:wait_value)    { 5 }
+
+  let(:blocking_proc) { Proc.new { context.wait(wait_target) } }
+
+  def dispatch_blocking_result
+    context.dispatch(wait_target, wait_value)
+  end
+
   describe '#initialize' do
+
+    it "delegates to super to set the context" do
+      expect(subject.context).to eq(context)
+    end
 
     it "sets the value_proc attribute" do
       expect(subject.value_proc).to eq(value_proc)
@@ -31,66 +44,59 @@ describe Xe::Enumerator::Strategy::Evaluator do
   describe '#call' do
 
     it "begins a fiber" do
-      expect(context).to receive(:begin_fiber).and_call_original
+      expect(context).to receive(:begin_fiber).once.and_call_original
       subject.call
     end
 
     context "within a new fiber" do
       let(:value_proc) { Proc.new { @eval_fiber = ::Fiber.current; value } }
 
-      it "executes value_proc" do
+      it "calls value_proc" do
         subject.call
         expect(@eval_fiber).to eq(context.last_fiber)
       end
     end
 
-    context "when eval_proc doesn't block" do
+    context "when eval_proc doesn't wait" do
       let(:value_proc) { Proc.new { value } }
       let(:value)      { 4 }
 
-      before do
-        @result = subject.call
+      it "returns the expected result" do
+        result = subject.call
+        expect(result).to eq(value)
       end
 
-      it "returns the result value" do
-        expect(@result).to eq(4)
-      end
-
-      it "terminates the fiber" do
+      it "allows the fiber to terminate" do
+        subject.call
         expect(context.last_fiber).to_not be_alive
       end
     end
 
-    context "when eval_proc blocks" do
-      let(:wait_target)   { Xe::Target.new(deferrable, 0, 0) }
-      let(:value_proc)    { Proc.new { context.wait(wait_target) } }
-      let(:target_value)  { 5 }
-
-      let(:finalize_proc) do
-        Proc.new { context.dispatch(wait_target, target_value) }
-      end
+    context "when eval_proc waits once" do
+      let(:value_proc)    { blocking_proc }
+      let(:finalize_proc) { Proc.new { dispatch_blocking_result } }
 
       before do
         @result = subject.call
       end
 
-      it "returns a proxy instance" do
+      it "substitutes a proxy for the result" do
         expect(is_proxy?(@result)).to be_true
       end
 
-      context "when wait_target's value becomes available" do
+      context "when the attended (waited) value becomes available" do
         it "calls dispatch on the context" do
           expect(context).to receive(:dispatch)
-          context.dispatch(wait_target, target_value)
+          context.dispatch(wait_target, wait_value)
         end
 
-        it "sets the value of the proxy" do
-          context.dispatch(wait_target, target_value)
-          expect(@result.subject).to eq(target_value)
+        it "resolves the subject of the proxy" do
+          context.dispatch(wait_target, wait_value)
+          expect(@result.subject).to eq(wait_value)
         end
       end
 
-      context "when there are fibers waiting on the result" do
+      context "when there is a fiber waiting on the result" do
         before do
           @resolve_value = nil
           @resolve_fiber = context.begin_fiber do
@@ -99,25 +105,25 @@ describe Xe::Enumerator::Strategy::Evaluator do
         end
 
         it "releases the fiber" do
-          context.dispatch(wait_target, target_value)
+          context.dispatch(wait_target, wait_value)
           expect(@resolve_fiber).to_not be_alive
         end
 
         it "releases the fiber with the correct value" do
-          context.dispatch(wait_target, target_value)
-          expect(@resolve_value).to eq(5)
+          context.dispatch(wait_target, wait_value)
+          expect(@resolve_value).to eq(wait_value)
         end
       end
 
-      context "when the proxy is resolved outside of a managed fiber" do
-        it "calls finalize! on the context" do
+      context "when outside of a managed fiber" do
+        it "calls finalize! on the context when the proxy is resolved" do
           expect(context).to receive(:finalize!)
           @result.resolve
         end
 
-        it "sets the value of the proxy" do
-          context.dispatch(wait_target, target_value)
-          expect(@result.subject).to eq(target_value)
+        it "sets the subject of the proxy when the proxy is resolved" do
+          @result.resolve
+          expect(@result.subject).to eq(wait_value)
         end
       end
     end
